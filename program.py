797dd4726d5c2866ca01acafc23ba8a72ba2c5ba
nikorai-prog <kolia9038072204@gmail.com>
2023-11-21 19:07:45 +0300

    awesome print

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
+    print('What an awesome print!')
 
 
 if __name__ == '__main__':
